c066b6afe62bcb06e14837f5862652026013d0cb
kanthreddy <kanthreddy1415@gmail.com>
2022-02-24 00:02:05 -0700

    adding from Local after AWS

--- one.java
+++ one.java
@@ -3,3 +3,4 @@ this file change is from git
 this is the one java file
 this change is added from the powershell using the notepad
 doing another change in powershell
+now adding a file after adding in AWS
